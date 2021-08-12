8d5097091ed7e945c182fdd310d524ac5ea875ad
Gene Carbone <gene.carbone@mulesoft.com>
2021-08-12 10:58:35 -0400

    basic CRUD with upvotes completed

--- force-app/main/default/lwc/proposal/proposal.js
+++ force-app/main/default/lwc/proposal/proposal.js
@@ -1,38 +1,121 @@
 import { LightningElement, api, track } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent'
+import { updateRecord,createRecord } from 'lightning/uiRecordApi';
+import PROPOSAL_OBJECT from '@salesforce/schema/Proposal__c';
+import ID_FIELD from '@salesforce/schema/Proposal__c.Id';
+import NAME_FIELD from '@salesforce/schema/Proposal__c.Name';
+import UPVOTES_FIELD from '@salesforce/schema/Proposal__c.Upvotes__c';
+import STAGE_FIELD from '@salesforce/schema/Proposal__c.Stage__c';
+import SYNOPSIS_FIELD from '@salesforce/schema/Proposal__c.Synopsis__c';
+import TickerSymbol from '@salesforce/schema/Account.TickerSymbol';
+
+
 
 export default class Proposal extends LightningElement {
    
 
     @api
-    prop = null;
+    prop = this.initRecord();
 
-    editMode = false;
+    editMode = true;
 
-    get editMode() {
-        return this.editMode;
+
+
+    initRecord(){
+        const fields = {};
+        fields[ID_FIELD.fieldApiName]='';
+        fields[NAME_FIELD.fieldApiName]='';
+        fields[UPVOTES_FIELD.fieldApiName]=0;
+        fields[STAGE_FIELD.fieldApiName]='New';
+        fields[SYNOPSIS_FIELD.fieldApiName]='';
+
+        return {fields};
     }
-    
     connectedCallback(){
-        console.log(JSON.stringify(this.prop));
+        console.log('in proposal');
     }
     
     upVote(){
+        var votes = parseInt(this.template.querySelector("[data-id='upvotes']").label) + 1
         console.log('click!');
-        console.log(this.prop.Upvotes__c);
-        this.prop.Upvotes__c = this.prop.Upvotes__c + 1;
-        console.log(this.prop.Upvotes__c);
+        
+        this.template.querySelector("[data-id='upvotes']").label = votes
+        console.log(votes);
         this.render();
         
     }
 
-    handleEditClick() {
-        this.editMode = !this.editMode;
+    closeDetail(event){
+
+        this.dispatchEvent(new CustomEvent("closedetail",{detail:{action:'close',record:[]}}));
     }
 
+    saveDetail(event){
+        const fields = {};
+        fields[ID_FIELD.fieldApiName]=this.prop.Id;
+        fields[NAME_FIELD.fieldApiName]=this.template.querySelector("[data-id='name']").value;
+        fields[UPVOTES_FIELD.fieldApiName]=this.template.querySelector("[data-id='upvotes']").label;
+        fields[STAGE_FIELD.fieldApiName]=this.prop.Stage__c;
+        fields[SYNOPSIS_FIELD.fieldApiName]=this.template.querySelector("[data-id='synopsis']").value;
+
+        
+        
+        if (this.prop.Id != null) {
+            const recordInput={fields};
+            updateRecord(recordInput)
+                .then(() => {
+                    this.dispatchEvent(
+                        new ShowToastEvent({title: 'Success',message: 'Proposal updated',variant: 'success'
+                        })
+                    );
+                    const closeDetailEvent = new CustomEvent("closedetail",{detail:{action:'update',record:fields}});
+                   
+                    this.dispatchEvent(closeDetailEvent);
+                    
+                    
+                })
+                .catch(error => {
+                    this.dispatchEvent(
+                        new ShowToastEvent({title: 'Error updating Proposal',message: error.body.message,variant: 'error'
+                        })
+                    );
+                });
+        } else {
+            const recordInput={apiName: PROPOSAL_OBJECT.objectApiName, fields};
+            fields[UPVOTES_FIELD.fieldApiName]=0;
+            fields[STAGE_FIELD.fieldApiName]='New';
+            console.log(recordInput);
+            createRecord(recordInput)
+                .then(() => {
+                    this.dispatchEvent(
+                        new ShowToastEvent({title: 'Success',message: 'Proposal created!',variant: 'success'
+                        })
+                    );
+                    const closeDetailEvent = new CustomEvent("closedetail",{detail:{action:'create',record:fields}});
+                    
+                    this.dispatchEvent(closeDetailEvent);
+                    console.log('created');
+                    
+                })
+                .catch(error => {
+                    var errmsg = error.body.message;
+                    console.log('create error' + errmsg);
+                    
+                    this.dispatchEvent(
+                        new ShowToastEvent({title: 'Error creating Proposal', message: errmsg, variant: 'error'
+                        })
+                    );
+                });
+        }
+        console.log('saving!');
+
+    }
+
+
+
     handleSaveClick() {
-        this.editMode = !this.editMode;
-        this.prop.synopsis = this.template.querySelector("[data-id='synopsis']").value;
+        
+        this.prop.Synopsis__c = this.template.querySelector("[data-id='synopsis']").value;
         
         const event = new ShowToastEvent({
             variant: 'success',
--- force-app/main/default/lwc/proposalSearch/proposalSearch.js
+++ force-app/main/default/lwc/proposalSearch/proposalSearch.js
@@ -1,7 +1,11 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, api, track } from 'lwc';
+import { deleteRecord } from 'lightning/uiRecordApi';
 import getProposalList from '@salesforce/apex/ProposalController.getProposalList';
+import { refreshApex } from '@salesforce/apex';
+
+
 const actions = [
-    { label: 'Show details', name: 'show_details' },
+    { label: 'Show / Edit details', name: 'show_details' },
     { label: 'Delete', name: 'delete' },
 ];
 
@@ -18,27 +22,38 @@ const columns = [
     
 ];
 export default class ProposalSearch extends LightningElement {
-    
-    data = [];
+    stageValue = '';
+    namesearch='';
+    showRecordDetail = false;
+    @api
+    columndata = [];
     columns = columns;
     record = {};
+    _proposalList = [];
 
-    @wire(getProposalList)
-    proposals({ error, data }) {
-        if (data) {
-            this.data = data;
+    @wire(getProposalList,{name:'$namesearch', stage:'$stageValue'})
+    proposals(result) {
+        this._proposalList = result;
+        if (result.data) {
+            this.columndata = result.data.map(x =>{
+                return {
+                 "Id": x.Id,
+                 "Name": x.Name,
+                 "Upvotes__c": x.Upvotes__c,
+                 "Stage__c": x.Stage__c,
+                 "Synopsis__c": x.Synopsis__c,
+                 "OwnerName": x.Owner.Name
+                };
+               });
             this.error = undefined;
-         console.log(JSON.stringify(data));
-        } else if (error) {
-            this.error = error;
-            this.data = undefined;
-        console.log(JSON.stringify(error));
+            console.log(JSON.stringify(result.data));
+        } else if (result.error) {
+            this.error = result.error;
+            this.columndata = undefined;
+            console.log(JSON.stringify(error));
+            this._proposalList = [];
         }
     };
-
-
-
-
 
     handleRowAction(event) {
         const actionName = event.detail.action.name;
@@ -55,19 +70,27 @@ export default class ProposalSearch extends LightningElement {
     }
 
     deleteRow(row) {
-        const { id } = row;
-        const index = this.findRowIndexById(id);
+        const { Id } = row;
+        const index = this.findRowIndexById(Id);
+        console.log('deleting ' + Id);
+        deleteRecord(Id)
+            .then(() => {
+                refreshApex(this.proposalList);
+              })
+              .catch(error => {
+              })
         if (index !== -1) {
-            this.data = this.data
+            this.columndata = this.columndata
                 .slice(0, index)
-                .concat(this.data.slice(index + 1));
+                .concat(this.columndata.slice(index + 1));
         }
     }
 
     findRowIndexById(id) {
         let ret = -1;
-        this.data.some((row, index) => {
-            if (row.id === id) {
+        
+        this.columndata.some((row, index) => {
+            if (row.Id === id) {
                 ret = index;
                 return true;
             }
@@ -78,5 +101,57 @@ export default class ProposalSearch extends LightningElement {
 
     showRowDetails(row) {
         this.record = row;
+        this.showRecordDetail = true;
+        console.log('show record detail' + JSON.stringify(row));
+    }
+
+    get stageOptions() {
+        return [
+            { label: 'All Stages', value: '' },
+            { label: 'New', value: 'New' },
+            { label: 'In Voting Process', value: 'In Voting Process' },
+            { label: 'Under Review', value: 'Under Review' },
+            { label: 'Accepted', value: 'Accepted' },
+            { label: 'Rejected', value: 'Rejected' },
+        ];
+    }
+
+    closeDetail(event){
+        this.showRecordDetail = false;
+        var edata = event.detail;
+        var erec = edata.record;
+        var recId = erec.Id;
+  
+        if (edata.action == 'update'){
+            var index = this.findRowIndexById(recId);
+            this.columndata[index].Name = erec.Name;
+            this.columndata[index].Upvotes__c = erec.Upvotes__c;
+            this.columndata[index].Synopsis__c = erec.Synopsis__c;
+            console.log(JSON.stringify(this.columndata));
+            this.refreshData();
+        }
+        if (edata.action != 'close'){
+            console.log("in refresh");
+            refreshApex(this._proposalList);
+        }
+
+    }
+
+      // in order to refresh your columndata, execute this function:
+    refreshData(){
+        this.columndata = JSON.parse(JSON.stringify(this.columndata));
+    }
+
+    handleNewButton(event) {
+        this.record = {};
+        this.showRecordDetail = true;
+    }
+
+    handleStageChange(event) {
+        this.stageValue = event.detail.value;
+    }
+
+    handleNamesearchChange(event){
+        this.namesearch = event.target.value;
     }
 }
